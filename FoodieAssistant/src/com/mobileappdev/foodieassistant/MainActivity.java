7930d18739b85bcec3b0b0e46fc9e1653f07b416
skim <skim@skims-macbook-pro.local>
2013-06-19 17:48:46 +0800

    + Second activity and its .xml file * Main activity and add intent to the ImageButton

--- FoodieAssistant/src/com/mobileappdev/foodieassistant/MainActivity.java
+++ FoodieAssistant/src/com/mobileappdev/foodieassistant/MainActivity.java
@@ -1,23 +1,46 @@
 package com.mobileappdev.foodieassistant;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-	setContentView(R.layout.activity_main);
-	// I modified this line.
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-	// Inflate the menu; this adds items to the action bar if it is present.
-	getMenuInflater().inflate(R.menu.main, menu);
-	return true;
-    }
+		ImageView startUpPicture = (ImageView) findViewById(R.id.startUpPicture);
 
+		// set onClickListener for ImageView
+		startUpPicture.setOnClickListener(new newActivityListener());
+
+		// start second layout
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	// Inner class for create a listener for OnClickListener
+	public class newActivityListener implements OnClickListener {
+		private static final int RESULT_CODE = 1;
+
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Intent intent = new Intent();
+			intent.setClass(MainActivity.this, SecondActivity.class);
+			startActivityForResult(intent, RESULT_CODE);
+		}
+	}
 }
---
+++ FoodieAssistant/src/com/mobileappdev/foodieassistant/SecondActivity.java
@@ -0,0 +1,27 @@
+package com.mobileappdev.foodieassistant;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class SecondActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_second);
+
+		Intent intent = getIntent();
+		Bundle bundle = intent.getExtras();
+		// bundle.getString();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.second, menu);
+		return true;
+	}
+
+}
