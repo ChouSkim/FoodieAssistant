35e44868142dcf13f533a03101be377a8cb741ba
Zhu <390086372@qq.com>
2013-06-19 22:04:27 +0800

    * add background to layouts.

--- FoodieAssistant/gen/com/mobileappdev/foodieassistant/R.java
+++ FoodieAssistant/gen/com/mobileappdev/foodieassistant/R.java
@@ -461,7 +461,8 @@ public final class R {
         public static final int textview_min_size=0x7f050003;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int backgroud=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
         public static final int LinearLayout1=0x7f090001;
@@ -469,14 +470,14 @@ public final class R {
         public static final int action_settings=0x7f09000d;
         public static final int button1=0x7f09000c;
         public static final int callNumber=0x7f090003;
-        public static final int imageView1=0x7f090005;
-        public static final int linearlayout1=0x7f090009;
-        public static final int linearlayout2=0x7f09000a;
-        public static final int listView1=0x7f090006;
-        public static final int loginButton=0x7f09000b;
-        public static final int loginTextView=0x7f090008;
+        public static final int imageView1=0x7f09000b;
+        public static final int linearlayout1=0x7f090008;
+        public static final int linearlayout2=0x7f090009;
+        public static final int listView1=0x7f090005;
+        public static final int loginButton=0x7f09000a;
+        public static final int loginTextView=0x7f090007;
         public static final int orderButton=0x7f090002;
-        public static final int startUpPicture=0x7f090007;
+        public static final int startUpPicture=0x7f090006;
         public static final int textView1=0x7f090004;
     }
     public static final class layout {
